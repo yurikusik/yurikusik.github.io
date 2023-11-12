78c67a2a89b36712d94c9b6cbfa4f47e344c2ff0
yurikusik <55029973+yurikusik@users.noreply.github.com>
2023-11-12 15:15:39 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -230,7 +230,7 @@ Author: Ahmed Beheiry
 			16 - Typed Text in Home Section
 	----------------------------------------------------- */
 	$(".typed-element").typed({
-    	strings: ["Performance Tester", "Automation Tester", "Manual Tester", "Software QA Enginner"],
+    	strings: ["Performance Tester", "Automation Tester", "Manual Tester", "Software Quality Enginner"],
     	typeSpeed: 10,
     	loop:true,
     	backDelay: 2000
